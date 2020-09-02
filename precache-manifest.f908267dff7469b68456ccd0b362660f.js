a7dbd09729502ebd370d4f24d43d6485a995cbf7
Dailer Hurtado <dailer.hurtado0321@gmail.com>
2020-09-02 11:19:26 -0500

    init build EV page

---
+++ precache-manifest.f908267dff7469b68456ccd0b362660f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6ec100968824ecce11a373086ab1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90623b3332945a0a8c92",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5814128a4a80403c4d4e",
+    "url": "/static/css/main.1416c473.chunk.css"
+  },
+  {
+    "revision": "90623b3332945a0a8c92",
+    "url": "/static/js/2.ed28e7fc.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/static/js/2.ed28e7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5814128a4a80403c4d4e",
+    "url": "/static/js/main.35950a46.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
